5cd16a4ee63dc365e064994f3322981c693592e6
落奈 <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2021-08-16 18:38:39 +0800

    update

--- float and double/main.c
+++ float and double/main.c
@@ -6,7 +6,9 @@ int main() {
      */
     float float_num = 2.13;
     double double_num = 3.14159;
-    printf("%f\n", float_num);
-    printf("%lf\n", double_num);
+    printf("%.8f\n", float_num);
+    printf("%.13f\n", float_num);
+    printf("%.15lf\n", double_num);
+    printf("%.17lf\n", double_num);
     return 0;
 }
