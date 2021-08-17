f7eb67fe9aaf4a5ab57f7f4911bb936b577514e1
落奈 <54143278+NoCodeNolife-cloud@users.noreply.github.com>
2021-08-17 22:25:54 +0800

    update

---
+++ waitpid/cmake-build-debug/CMakeFiles/waitpid.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/waitpid.dir/main.c.obj"
+  "libwaitpid.dll.a"
+  "waitpid.exe"
+  "waitpid.exe.manifest"
+  "waitpid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/waitpid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
